767d98e83aaa6ac7c3b1d2aced38aeed2116cda3
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-06-12 15:25:06 +0300

    Initial Commit

---
+++ Settings screen TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings screen TableView
+//
+//  Created by Anatoliy on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
