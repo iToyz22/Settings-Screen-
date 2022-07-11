fa81131745a263c6756abc208591265c91eee85f
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-07-11 21:46:18 +0300

    MVC HW15
    Add model, view, controller

---
+++ Settings screen TableView/Scenes/Controller/SettingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewController.swift
+//  Settings screen TableView
+//
+//  Created by Anatoliy on 11.07.2022.
+//
+
+import Foundation
---
+++ Settings screen TableView/Scenes/Model/Section.swift
@@ -0,0 +1,8 @@
+//
+//  Section.swift
+//  Settings screen TableView
+//
+//  Created by Anatoliy on 11.07.2022.
+//
+
+import Foundation
